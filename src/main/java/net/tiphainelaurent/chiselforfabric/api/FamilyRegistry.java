8b39b3d4df24ae1654df652d1e266260af86b13c
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-09-22 19:10:24 +0200

    Import Minecraft Block instead of Helpers Block

--- src/main/java/net/tiphainelaurent/chiselforfabric/api/FamilyRegistry.java
+++ src/main/java/net/tiphainelaurent/chiselforfabric/api/FamilyRegistry.java
@@ -4,30 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.util.Identifier;
 
-import net.tiphainelaurent.chiselforfabric.api.helpers.Item;
 import net.tiphainelaurent.chiselforfabric.api.helpers.Resource;
-import net.tiphainelaurent.chiselforfabric.api.helpers.Block;
 
 public abstract class FamilyRegistry {
     abstract public Set<Identifier> getBlocks();
     abstract public String getTextureDirectory();
-    abstract public net.minecraft.block.Block getAncestor();
-    abstract public Recipe<?> getRecipe(final net.minecraft.block.Block current);
-    abstract public Recipe<?> getReversedRecipe(final net.minecraft.block.Block parent, final net.minecraft.block.Block current);
+    abstract public Block getAncestor();
+    abstract public Recipe<?> getRecipe(final Block current);
+    abstract public Recipe<?> getReversedRecipe(final Block parent, final Block current);
 
     public void registerAll(final ItemGroup group)
     {
         final Set<Identifier> blockIds = getBlocks();
-        final List<net.minecraft.block.Block> blocks = new ArrayList<>(blockIds.size());
+        final List<Block> blocks = new ArrayList<>(blockIds.size());
 
         blockIds.forEach((blockId) -> {
             final String namespace = blockId.getNamespace();
             final String blockName = blockId.getPath();
-            net.minecraft.block.Block block = Block.builder(getAncestor())
+            Block block = net.tiphainelaurent.chiselforfabric.api.helpers.Block.builder(getAncestor())
                  .mineable()
                  .asItem(group)
                  .build(namespace, blockName);
@@ -38,10 +37,10 @@ public abstract class FamilyRegistry {
         });
 
         blocks.forEach((parent) -> {
-            Item.builder().withRecipe(getRecipe(parent));
+            net.tiphainelaurent.chiselforfabric.api.helpers.Item.builder().withRecipe(getRecipe(parent));
 
             blocks.forEach((block) -> {
-                Item.builder().withRecipe(getReversedRecipe(parent, block));
+                net.tiphainelaurent.chiselforfabric.api.helpers.Item.builder().withRecipe(getReversedRecipe(parent, block));
             });
         });
     }
