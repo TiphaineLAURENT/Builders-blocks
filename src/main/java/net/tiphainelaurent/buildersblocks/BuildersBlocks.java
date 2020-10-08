48938b323ae744bd6d01cc90eefdef67fe3f32d0
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-10-08 22:19:10 +0200

    Add all vanilla blocks

--- src/main/java/net/tiphainelaurent/buildersblocks/BuildersBlocks.java
+++ src/main/java/net/tiphainelaurent/buildersblocks/BuildersBlocks.java
@@ -48,8 +48,69 @@ public class BuildersBlocks implements ModInitializer
 		} catch (Exception e) {
 			LOGGER.error(e);
 		}
+		// new GenericFamilyRegistry("ancient_stone", Blocks.STONE).registerAll(ITEM_GROUP);
 		new AndesiteFamily().registerAll(ITEM_GROUP);
-		new GenericFamilyRegistry("ancient_stone", Blocks.STONE).registerAll(ITEM_GROUP);
 		new AntiBlockFamily().registerAll(ITEM_GROUP);
+		// new GenericFamilyRegistry("arcane_stone", Blocks.STONE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("basalt", Blocks.BASALT).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("bricks", Blocks.BRICKS).registerAll(ITEM_GROUP);
+		// new GenericFamilyRegistry("certus", Blocks.QUARTZ_BLOCK).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("coal/charcoal", Blocks.COAL_BLOCK).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("coal/coal", Blocks.COAL_BLOCK).registerAll(ITEM_GROUP);
+		// new GenericFamilyRegistry("coal/coalcoke", Blocks.COAL_BLOCK).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("cobblestone", Blocks.COBBLESTONE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("concrete_black", Blocks.BLACK_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_blue", Blocks.BLUE_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_brown", Blocks.BROWN_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_cyan", Blocks.CYAN_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_gray", Blocks.GRAY_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_green", Blocks.GREEN_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_lightblue", Blocks.LIGHT_BLUE_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_lightgray", Blocks.LIGHT_GRAY_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_lime", Blocks.LIME_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_magenta", Blocks.MAGENTA_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_orange", Blocks.ORANGE_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_pink", Blocks.PINK_CONCRETE).registerAll(ITEM_GROUP);
+		// new GenericFamilyRegistry("concrete_powder", Blocks.WHITE_CONCRETE_POWDER).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_purple", Blocks.PURPLE_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_red", Blocks.RED_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_white", Blocks.WHITE_CONCRETE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("concrete_yellow", Blocks.YELLOW_CONCRETE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("diorite", Blocks.DIORITE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("endstone", Blocks.END_STONE).registerAll(ITEM_GROUP);
+
+		// new GenericFamilyRegistry("fluid", Blocks.STONE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("glowstone", Blocks.GLOWSTONE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("granite", Blocks.GRANITE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("hardenedclay", Blocks.TERRACOTTA).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("ice", Blocks.ICE).registerAll(ITEM_GROUP);
+
+		// new GenericFamilyRegistry("limestone", Blocks.STONE).registerAll(ITEM_GROUP);
+		// new GenericFamilyRegistry("marble", Blocks.QUARTZ_BLOCK).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("magma", Blocks.MAGMA_BLOCK).registerAll(ITEM_GROUP);
+
+		// new GenericFamilyRegistry("primarineanim", Blocks.PRISMARINE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("purpur", Blocks.STONE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("quartz", Blocks.QUARTZ_BLOCK).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("redstone", Blocks.REDSTONE_BLOCK).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("sandstonered", Blocks.RED_SANDSTONE).registerAll(ITEM_GROUP);
+		new GenericFamilyRegistry("sandstoneyellow", Blocks.SANDSTONE).registerAll(ITEM_GROUP);
+
+		new GenericFamilyRegistry("stone", Blocks.STONE).registerAll(ITEM_GROUP);
+		
 	}
 }
